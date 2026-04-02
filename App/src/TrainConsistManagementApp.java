ce34e535c31a84c39ae65efbe1c793f6d1d3adbe
squirrelk6755-ctrl <squirrelk6755@gmail.com>
2026-04-02 15:08:43 +0530

    Muskan

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,7 +1,35 @@
-public class TrainConsistManagementApp {
+import java.util.HashSet;
+import java.util.Set;
+
+public class TrainConsistManagementApp{
+
     public static void main(String[] args) {
-        System.out.println("=======================================");
-        System.out.println("        Welcome to " + "TrainConsistManagementApp");
-        System.out.println("=======================================");
+
+        System.out.println("========================================");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println("========================================\n");
+
+        // Create a Set to store unique bogie IDs
+        // HashSet stores only unique values
+        Set<String> bogies = new HashSet<>();
+
+        // ---- ADD IDs (including duplicates) ----
+        // add() inserts bogie IDs into the set
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        // Duplicate entries will be ignored internally by HashSet
+        bogies.add("BG101"); // Duplicate entry
+        bogies.add("BG102"); // Duplicate entry
+
+        // ---- DISPLAY unique bogie IDs ----
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
+
+        System.out.println("\nNote:");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
+
+        System.out.println("\nUC3 uniqueness validation completed...");
     }
 }
