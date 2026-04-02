9a0a99a572bc9b0f380d23d98d477b9480bc6fff
squirrelk6755-ctrl <squirrelk6755@gmail.com>
2026-04-02 15:30:35 +0530

    Muskan

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,7 +1,29 @@
-public class TrainConsistManagementApp {
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrainConsistManagementApp{
+
     public static void main(String[] args) {
-        System.out.println("=======================================");
-        System.out.println("        Welcome to " + "TrainConsistManagementApp");
-        System.out.println("=======================================");
+
+        System.out.println("========================================");
+        System.out.println(" UC6 - Map Bogie to Capacity (HashMap) ");
+        System.out.println("========================================\n");
+
+        // HashMap stores data in key -> value format
+        Map<String, Integer> capacityMap = new HashMap<>();
+
+        // ---- Insert bogie capacities ----
+        capacityMap.put("Sleeper", 72);
+        capacityMap.put("AC Chair", 56);
+        capacityMap.put("First Class", 24);
+        capacityMap.put("Cargo", 120);
+
+        // ---- Display bogie capacity details ----
+        System.out.println("Bogie Capacity Details:");
+        for (Map.Entry<String, Integer> entry : capacityMap.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        System.out.println("\nUC6 bogie-capacity mapping completed...");
     }
 }
